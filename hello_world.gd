6244c4284f5390e7a02ce190ee3c6532b59a16e6
mvetere1@binghamton.edu <58411412+mvetere1@users.noreply.github.com>
2026-01-29 20:44:19 -0500

    Initial commit

---
+++ hello_world.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	$Label.text = "Hello, Godot!"
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
+	$Label2.text = "Hello, button2"
+	$Label3.text = "Hello, button3"
+	pass # Replace with function body.
