5d5cba1585418ce40553f2d33fafcb76289afa49
mvetere1@binghamton.edu <58411412+mvetere1@users.noreply.github.com>
2026-01-31 13:43:11 -0500

    fixed the solaria demo tile file, fixed char movement so it only coincides with actually moving
    to fix the .png, just copy and paste it into a new folder.  old folder was downloaded from the web and mac put quarentine flag on it, making it unable to open

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -16,14 +16,16 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$AnimatedSprite2D.play("Loop")
-
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.play("Loop")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.stop()
 
 	move_and_slide()
