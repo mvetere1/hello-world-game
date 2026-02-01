239630aa5e61838f436369f859f71d4125e554c9
Matthew Matyus <matthewmatyus@Matthews-MacBook-Pro-2.local>
2026-02-01 00:26:58 -0800

    cocaine block for speed and jump

--- basicplatformercs2/character_body_2d.gd
+++ basicplatformercs2/character_body_2d.gd
@@ -1,9 +1,8 @@
 extends CharacterBody2D
 
-
-const SPEED = 400.0
-const JUMP_VELOCITY = -600.0
-
+const BASE_SPEED = 400.0
+var speed = BASE_SPEED
+var jump_velocity = -600.0
 
 
 func _physics_process(delta: float) -> void:
@@ -14,7 +13,7 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 		$AnimatedSprite2D.play("Loop")
 
 
@@ -22,10 +21,10 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 		$AnimatedSprite2D.play("Loop")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 		$AnimatedSprite2D.stop()
 
 	move_and_slide()
